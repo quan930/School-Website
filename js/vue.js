bb2c77a374b379048a87c2d6ba0e33301619a38e
quan930 <1583090876@qq.com>
2019-03-05 22:00:24 +0800

    second commit

---
+++ js/vue.js
@@ -0,0 +1,40 @@
+new Vue({
+    el: '#app',
+    data: {
+        imgList:[
+            {
+                url:"image/carousel06.jpg"
+            },
+            {
+                url:"image/carousel07.jpg"
+            },
+            {
+                url:"image/carousel09.jpg"
+            },
+            {
+                url:"image/carousel08.jpg"
+            },
+            {
+                url:"image/carousel05.jpg"
+            }
+        ],
+        smallImgList:[
+            {
+                mes:"召开教学工作会议暨师德师风建设工作会议",
+                url:"image/carousel_small_01.jpg"
+            },
+            {
+                mes:"党委书记王冠教授给学生上“形势与政策”课",
+                url:"image/carousel_small_02.jpg"
+            },
+            {
+                mes: "我校召开思想政治工作推进会议",
+                url:"image/carousel_small_03.jpg"
+            },
+            {
+                mes: "我校召开2018年全面从严治党工作会议",
+                url:"image/carousel_small_04.jpg"
+            }
+        ]
+    }
+});
